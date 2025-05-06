4d888e2bf1bb057171b15f7de92059852df9b6ad
dayth36386 <dechawiwatwong.d@gmail.com>
2025-05-06 09:40:22 +0700

    edit metatag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,24 +9,32 @@ export default function page() {
       <Head>
         <title>Dechawiwatwong Detchang</title>
         <meta name="title" content="Dechawiwatwong Detchang" />
-        <meta
-          name="description"
-          content="I'm a Junior Front-End Developer passionate about building scalable web applications with Next.js, Node.js, and MongoDB. With hands-on experience in UX/UI design, I've participated in startup competitions and IoT projects. I'm currently pursuing a Bachelor of Science and Technology at Kasem Bundit University (2021 - 2025)."
-        />
+        <meta name="description" content="JUNIOR FRONTEND DEVELOPER" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="UTF-8" />
 
-        {/* Open Graph / Facebook */}
+        <meta
+          name="image"
+          content="https://i.postimg.cc/ZY20Tp08/Screenshot-2568-05-06-at-09-22-34.png"
+        />
+        <link
+          rel="image_src"
+          href="https://i.postimg.cc/ZY20Tp08/Screenshot-2568-05-06-at-09-22-34.png"
+        />
+
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://porfolio-gdyo.vercel.app/" />
-        <meta name="title" content="Portfolio Dechawiwatwong Detchang" />
-        <meta name="description" content="JUNIOR FRONTEND DEVELOPER" />
+        <meta property="og:title" content="Dechawiwatwong Detchang" />
+        <meta
+          property="og:description"
+          content="I'm a Junior Front-End Developer passionate about building scalable web applications..."
+        />
         <meta
           property="og:image"
           content="https://i.postimg.cc/ZY20Tp08/Screenshot-2568-05-06-at-09-22-34.png"
         />
 
-        {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta
           property="twitter:url"
@@ -35,13 +43,14 @@ export default function page() {
         <meta property="twitter:title" content="Dechawiwatwong Detchang" />
         <meta
           property="twitter:description"
-          content="I'm a Junior Front-End Developer passionate about building scalable web applications with Next.js, Node.js, and MongoDB. With hands-on experience in UX/UI design, I've participated in startup competitions and IoT projects. I'm currently pursuing a Bachelor of Science and Technology at Kasem Bundit University (2021 - 2025)."
+          content="I'm a Junior Front-End Developer passionate about building scalable web applications..."
         />
         <meta
           property="twitter:image"
           content="https://i.postimg.cc/ZY20Tp08/Screenshot-2568-05-06-at-09-22-34.png"
         />
       </Head>
+
       <HomePageRender />
     </>
   );
